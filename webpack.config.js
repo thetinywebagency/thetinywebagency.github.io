087cad857870dedb204991930322403926389a0b
justtoconfirm <office@justtoconfirm.co.uk>
2020-10-13 11:32:30 +0100

    Webpack Config updated to include NPM packages for compiling SCSS into CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,60 @@
 const env = process.env.NODE_ENV;
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
 		main: './src/index.js'
 	},
+	module: {
+		rules: [
+			{
+				test: /\.s[c]ss$/,
+				exclude: /node_modules/,
+				use: [
+					'style-loader',
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					'css-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			// Set the filename of the CSS stylesheet if production or development mode is used
+			filename: env === 'production' ? 'style/min/[name].min.css' : 'style/[name].css' 
+		})
+    ],
 	output: {
 		path: path.resolve(__dirname, './dist/'),
 		pathinfo: false,
